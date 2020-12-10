c808bc2283ef152b3000ad91044eaade168c0d12
Sergey <s.gz@yandex.ru>
2020-12-10 11:13:39 +0300

    Внес 5 строк print
    Это подробное описание

--- git_project1.py
+++ git_project1.py
@@ -2,7 +2,10 @@ def main():
     print('My first git program')
     for i in range(9):
         print("*" * i)
-
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 if __name__ == '__main__':
     main()
